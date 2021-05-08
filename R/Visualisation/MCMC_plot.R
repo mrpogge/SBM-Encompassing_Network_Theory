2556513b83b0639589f3cc8db7f8e1cf5531af4d
mrpogge <83350062+mrpogge@users.noreply.github.com>
2021-05-08 12:43:01 +0200

    Basics of a visual in a 4 cluster scenario

---
+++ R/Visualisation/MCMC_plot.R
@@ -0,0 +1,23 @@
+#first run the algorithm and save the mcmc output
+
+m <- matrix(0, iter, (K * (K+1))/2)
+
+
+for(i in 1:1000){
+ m[i, ] <- theta_c[[i]][lower.tri(theta_c[[i]], diag = T)]
+}
+
+
+library(ggplot2)
+library(gridExtra)
+
+m <- as.data.frame(m)
+
+
+
+par(mfrow = c(2,5))
+for(i in 1:ncol(m)){
+  plot(m[,i], type = "l")
+}
+
+
