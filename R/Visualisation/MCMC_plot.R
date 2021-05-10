586d21a9b5d608647f7ab15108c56a6a4d7049cb
mrpogge <83350062+mrpogge@users.noreply.github.com>
2021-05-10 23:12:54 +0200

    Visualisation wrtitten for the example (not in a function form yet)

--- R/Visualisation/MCMC_plot.R
+++ R/Visualisation/MCMC_plot.R
@@ -1,6 +1,7 @@
 #first run the algorithm and save the mcmc output
 
-m <- matrix(0, iter, (K * (K+1))/2)
+theta_c <- res$theta
+m <- matrix(0, 1000, (K * (K+1))/2)
 
 
 for(i in 1:1000){
@@ -40,37 +41,10 @@ m_hat[lower.tri(m_hat, diag = TRUE)] <- colMeans(m[400:1000,])
 m_hat
 Theta_true
 
-##########################↓ artificial identifiable constrain
+colMeans(m_atheta[200:1000,])
+Theta_true[lower.tri(Theta_true, diag = TRUE)]
 
-#simple IC 1>2>3>4>5>6
-
-m_reordered <- matrix(0, iter, (K * (K+1))/2)
-for(reorder in 1:nrow(m)){
-  m_reordered[reorder,] <- sort(m[reorder,], decreasing = TRUE)
-}
-
-
-
-par(mfrow = c(2,5))
-for(i in 1:ncol(m)){
-  plot(m_reordered[,i], type = "l")
-}
-
-
-
-
-par(mfrow = c(3, 5))
-for(i in 1:ncol(m)){
-
-  hist(m_reordered[,i])
-}
-
-
-colMeans(m_reordered)
-sort(Theta_true[lower.tri(Theta_true, diag = TRUE)], decreasing = TRUE)
-
-m_hat <- matrix(0, K, K)
-m_hat[lower.tri(m_hat, diag = TRUE)] <- colMeans(m_reordered[400:1000,])
+m_atheta_hat <- matrix(0, K, K)
+m_hat[lower.tri(m_atheta_hat, diag = TRUE)] <- colMeans(m_atheta[400:1000,])
 m_hat
 Theta_true
-
